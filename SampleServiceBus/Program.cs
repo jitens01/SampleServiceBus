53374b1ebffafffaa0c986e9bfa34f51ae8c4234
Jitendra Sankhla <jitens01@hotmail.com>
2017-10-25 11:26:30 +0530

    Add project files.

---
+++ SampleServiceBus/Program.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleServiceBus
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SendMessageUsingQueue();
+            RecieveMessageUsingQueue();
+        }
+
+        private static void SendMessageUsingQueue()
+        {
+            try
+            {
+                var connectionString = "Endpoint=sb://basicservicebus17oct.servicebus.windows.net/;SharedAccessKeyName=queue17oct;SharedAccessKey=v+j90RsKBlRnPaoqm/T4AURhgVHMauasNFpb0c5sScg=";
+                var queueName = "basicqueue17oct";
+
+                var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+                var message = new BrokeredMessage("This is a test message!");
+
+                Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+
+                client.Send(message);
+
+                Console.WriteLine("Message successfully sent! Press ENTER to continue..");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private static void RecieveMessageUsingQueue()
+        {
+            try
+            {
+                var connectionString = "Endpoint=sb://basicservicebus17oct.servicebus.windows.net/;SharedAccessKeyName=queue17oct;SharedAccessKey=v+j90RsKBlRnPaoqm/T4AURhgVHMauasNFpb0c5sScg=";
+                var queueName = "basicqueue17oct";
+
+                var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+                client.OnMessage(message =>
+                {
+                    Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
+                    Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+                });
+
+                Console.WriteLine("Press ENTER to exit program");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SampleServiceBusUsingTopics/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleServiceBusUsingTopics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SendMessageUsingTopics();
+            RecieveMessageUsingTopics();
+        }
+
+        private static void SendMessageUsingTopics()
+        {
+            try
+            {
+                var connectionString = "Endpoint=sb://basicservicebus17oct.servicebus.windows.net/;SharedAccessKeyName=topic17oct;SharedAccessKey=4g5f0mAGF7+Ty0i6/KLB26AvwekDE0wUZfX70rVDr4k=";
+                var topicName = "basictopic17oct";
+
+                var client = TopicClient.CreateFromConnectionString(connectionString, topicName);
+                var message = new BrokeredMessage("This is a test message!");
+
+                Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
+
+                Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+
+                client.Send(message);
+
+                Console.WriteLine("Message successfully sent! Press ENTER to continue..");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private static void RecieveMessageUsingTopics()
+        {
+            try
+            {
+                var connectionString = "Endpoint=sb://basicservicebus17oct.servicebus.windows.net/;SharedAccessKeyName=topic17oct;SharedAccessKey=4g5f0mAGF7+Ty0i6/KLB26AvwekDE0wUZfX70rVDr4k=";
+                var topicName = "basictopic17oct";
+
+                var client = SubscriptionClient.CreateFromConnectionString(connectionString, topicName, "JitendraMachine");
+
+                client.OnMessage(message =>
+                {
+                    Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
+                    Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+                });
+
+                Console.WriteLine("Press ENTER to exit program");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
